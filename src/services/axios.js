f22011c9df10a146f9c597c5f01312d4807acb09
unknown <aranchipe98@gmail.com>
2022-11-20 22:52:03 -0300

    desafio concluido

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3334",
+  baseURL: "https://drivopsapi.herokuapp.com",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
